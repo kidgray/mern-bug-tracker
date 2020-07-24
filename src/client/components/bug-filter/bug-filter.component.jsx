d43b3c58512b0f7e9bcaabb6e9f43ed3b634484c
Micah Allauca <micah.allauca@gmail.com>
2020-07-24 17:16:37 -0400

    Filter functionality has been implemented.

--- src/client/components/bug-filter/bug-filter.component.jsx
+++ src/client/components/bug-filter/bug-filter.component.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BugFilter = (props) => {
-    const handleClick = () => {
-        props.setFilter({ priority: 1 });
+    // State Hook for the Priority filter field
+    const [priority, setPriority] = useState('');
+
+    // State Hook for the Status filter field
+    const [status, setStatus] = useState('');
+
+    const handleSubmit = () => {
+        props.setFilter({ priority, status });
     }
 
     return (
         <div>
-            <button onClick={handleClick}>Test Filter Functionality</button>
+            <h2>List Filter</h2>
+
+            Status:
+            <select className="filter-field" value={priority} onChange={(event) => setPriority(event.target.value)}>
+                <option value=""> All </option>
+                <option value="1"> 1 </option>
+                <option value="2"> 2 </option>
+                <option value="3"> 3 </option>
+            </select>
+
+            Priority:
+            <select className="filter-field" value={status} onChange={(event) => setStatus(event.target.value)}>
+                <option value=""> All </option>
+                <option value="New"> New </option>
+                <option value="Open"> Open </option>
+                <option value="Closed"> Closed </option>
+            </select>
+
+            <button onClick={handleSubmit}>Test Filter Functionality</button>
         </div>
     );
 };
