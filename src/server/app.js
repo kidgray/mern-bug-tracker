b8160826f8eeef1f425c2a5abd41cb4b3cdf4448
kidgray <micah.allauca@gmail.com>
2020-08-19 20:13:32 -0400

    Fixed incorrect port issue with Heroku, but I don't think that was the problem

--- src/server/app.js
+++ src/server/app.js
@@ -14,7 +14,7 @@ let db;
 
 // Variables for Express connection
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve a static HTML file
 app.use(express.static('../../public'));
